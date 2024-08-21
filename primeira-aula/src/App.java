a39587f60cd969bcdd61f8f3a179f4f6a7575190
Estevam Cabral <58441670+estevamcabral@users.noreply.github.com>
2024-08-20 21:23:33 -0300

    Update App.java

--- primeira-aula/src/App.java
+++ primeira-aula/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Estevam Cabral Pacheco & Leonardo Chiao");
+        System.out.println("");
     }
 }
