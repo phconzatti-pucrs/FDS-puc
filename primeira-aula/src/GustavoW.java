b6f5b7a2131f50390810538af91b7b09ee6ab12c
Gustavo Fulber Wide <107080436+gwide@users.noreply.github.com>
2024-08-23 00:45:33 +0000

    gustavo f wide

---
+++ primeira-aula/src/GustavoW.java
@@ -0,0 +1,3 @@
+public class GustavoW {
+    System.out.println("Gustavo W");
+}
--- primeira-aula/src/GustavoWide.java
+++
@@ -1,5 +0,0 @@
-public class GustavoWide(){
-    public static void main(string[] args){
-        System.out.println("Gustavo Fulber Wide");
-    }
-}
